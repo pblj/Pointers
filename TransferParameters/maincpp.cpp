289a47170f242b1df5a6374937b47ceb848bf73a
pblj <shkabarev.av@gmail.com>
2025-04-12 15:21:01 +0500

    transfer pointerrs

--- TransferParameters/maincpp.cpp
+++ TransferParameters/maincpp.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int* a, int* b);
 
 void main()
 {
@@ -10,14 +10,14 @@ void main()
 
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
-	Exchange(a, b);
+	Exchange(&a, &b);
 	cout << a << "\t" << b << endl;
 
 }
 
-void Exchange(int a, int b)
+void Exchange(int* a, int* b)
 {
-	int buffer = a;
-	a = b;
-	b = buffer;
+	int buffer = *a;
+	*a = *b;
+	*b = buffer;
 }
